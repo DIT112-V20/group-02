ffcf865c87a9ae4403ed6c6da689c63b3d1e4584
Linus Ivarsson <linusivarsson@live.se>
2020-05-13 14:11:45 +0200

    Added funcionality for back-button on Help-page

--- app/app/src/main/java/com/example/app/HelpActivity.kt
+++ app/app/src/main/java/com/example/app/HelpActivity.kt
@@ -2,16 +2,20 @@ package com.example.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_help.*
 
 class HelpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
+        buttonExit.setOnClickListener{back()}
     }
 
-    fun back(){
+    private fun back() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
