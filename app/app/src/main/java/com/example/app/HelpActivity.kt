dda55d0199780b35d98e29f117e65970aed8fc10
clementinejensen <gusjensecl@gmail.com>
2020-05-13 11:56:17 +0200

    Add help activity
    Added the help activity, and allowed access to the help activity.

---
+++ app/app/src/main/java/com/example/app/HelpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HelpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+    }
+
+}
